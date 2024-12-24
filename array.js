8f402dd9f7792d23b21af82a2860c397f3c0743b
mskl-kashish <kashishrawat308@gmail.com>
2024-12-24 12:50:40 +0530

    wrote a method to seprate 0 and 1 in an array

---
+++ array.js
@@ -0,0 +1,31 @@
+// separate 0 and 1
+function separateZeroAndOne(arr) {
+  let i = 0;
+  let j = arr.length - 1;
+  console.log(arr);
+  while (i <= j) {
+    // if (arr[i] == 0) {
+    //   i++;
+    // }
+    // if (arr[i] == 1) {
+    //   let a = arr[j];
+    //   arr[j] = arr[i];
+    //   arr[i] = a;
+    //   j--;
+    //   if (a == 0) {
+    //     i++;
+    //   }
+    // }
+    if (arr[i] == 1) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      j--;
+    } else {
+      i++;
+    }
+  }
+  return arr;
+}
+
+console.log(separateZeroAndOne([1, 0, 0, 1, 0, 1, 1, 0, 0]));
