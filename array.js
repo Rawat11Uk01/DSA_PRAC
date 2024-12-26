08c44055c700dd3b59acd4d08012239ab46510df
mskl-kashish <kashishrawat308@gmail.com>
2024-12-26 17:15:10 +0530

    feat: Add functions to reverse an array and merge two sorted arrays

--- array.js
+++ array.js
@@ -43,4 +43,49 @@ const repeatedChar = (string) => {
   return stringCount;
 };
 
-console.log(repeatedChar("kashish"));
+// console.log(repeatedChar("kashish"));
+
+// reverse an array
+
+function reverseArray(array) {
+  for (let idx = 0; idx < Math.floor(array.length / 2); idx++) {
+    let temp = array[idx];
+    array[idx] = array[array.length - 1 - idx];
+    array[array.length - 1 - idx] = temp;
+  }
+  return array;
+}
+
+// console.log(reverseArray([1, 2, 3, 4, 5]));
+
+//merge sorted array
+
+function mergeSortedArray(a, b) {
+  let c = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < a.length && j < b.length) {
+    if (a[i] < b[j]) {
+      c.push(a[i]);
+      i++;
+    } else {
+      c.push(b[j]);
+      j++;
+    }
+  }
+
+  // Add remaining elements from `a` or `b` if any
+  while (i < a.length) {
+    c.push(a[i]);
+    i++;
+  }
+
+  while (j < b.length) {
+    c.push(b[j]);
+    j++;
+  }
+
+  return c;
+}
+console.log(mergeSortedArray([1, 2, 4, 9, 11], [0, 3, 4, 6]));
